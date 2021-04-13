6ead593eaffa2e6772573db1a16da2cd1165d64b
sangi <sssangeetha494@gmail.com>
2021-04-13 15:12:18 -0500

    new commit

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 // TODO: add code here
+window.addEventListener("load",function()
+{
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response)
+    {    
+
+        response.json().then( function(data) 
+        {
+         console.log(data);
+         const containers=document.getElementById("container");
+         for(let i = 0; i< data.length; i++)
+            {
+            
+            // console.log(json[i].active);
+            containers.innerHTML += `<div class="astronaut">
+               <div class="bio">
+                  <h3>${data[i].firstName}</h3>
+                  <ul>
+                     <li>Hours in space: ${data[i].hoursInSpace}</li>
+                     <li>Active: ${data[i].active}</li>
+                     <li>Skills: ${data[i].skills}</li>
+                  </ul>
+               </div>
+               <img class="avatar" src="${data[i].picture}"></img>
+            </div>`;   
+            }
+        
+         });
+      });
+   // });
+});
